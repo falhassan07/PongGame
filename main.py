7dbf6bb6b1e684fb2cc29f03675486608921a8f2
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-11-16 04:32:03 -0500

    added left paddle

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0) #turn off animation
 
-paddle_right = Paddle()
+right_paddle = Paddle((350,0))
+left_paddle = Paddle((-350,0))
 
 
 screen.listen()
-screen.onkey(paddle_right.up, "Up")
-screen.onkey(paddle_right.down, "Down")
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
 
 
 game_is_on = True 
--- paddle.py
+++ paddle.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.paddle = Turtle()
         # self.paddle.speed(0)
@@ -10,7 +10,7 @@ class Paddle(Turtle):
         self.paddle.shape("square")
         self.paddle.shapesize(5, 1)
         self.paddle.color("white")
-        self.paddle.goto(350,0)
+        self.paddle.goto(position)
     
     def up(self):
         new_y = self.paddle.ycor() + 20
